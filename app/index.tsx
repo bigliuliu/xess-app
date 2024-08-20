145957c9137ea60b741e63e1ad922646c106cf9f
Chris Yu <yuetong3yu@gmail.com>
2024-08-20 16:27:46 +1000

    remove Stack

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <StatusBar style="light" />
+      <Link href="/welcome">Welcome Page</Link>
     </View>
-  );
+  )
 }
