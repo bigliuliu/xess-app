07fef5c62a90dd4c4406ac503f725e2cac23857e
Chris Yu <yuetong3yu@gmail.com>
2024-08-22 11:17:21 +1000

    feat: button styles

--- components/BigButton.tsx
+++ components/BigButton.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, PropsWithChildren } from 'react'
+import { CSSProperties, PropsWithChildren, useState } from 'react'
 import { StyleSheet, Text, Pressable, StyleProp } from 'react-native'
 
 export function BigButton({
@@ -6,24 +6,33 @@ export function BigButton({
   text,
   buttonStyle,
 }: PropsWithChildren & { text: string; buttonStyle?: Object }) {
+  const [isPress, setIsPress] = useState(false)
+
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: '#000000',
+      width: 353,
+      height: 52,
+      borderRadius: 26,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: 16,
+      fontWeight: 500,
+      opacity: isPress ? 0.5 : 1,
+    },
+    text: {
+      color: '#ffffff',
+    },
+  })
+
   return (
-    <Pressable style={[styles.button, buttonStyle || {}]}>
+    <Pressable
+      style={[styles.button, buttonStyle || {}]}
+      onPressIn={() => setIsPress(true)}
+      onPressOut={() => setIsPress(false)}
+    >
       <Text style={styles.text}>{text}</Text>
     </Pressable>
   )
 }
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#000000',
-    width: 353,
-    height: 52,
-    borderRadius: 26,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#ffffff',
-  },
-})
