d27bb45fbb94b7c74680cd4cd0003e2fec793e60
Chris Yu <yuetong3yu@gmail.com>
2024-08-24 17:27:52 +1000

    use stack to navigate between pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar'
-import { Slot } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Stack } from 'expo-router'
 
 export default function RootLayout() {
   return (
     <>
       <StatusBar style="auto" />
       <SafeAreaView style={{ flex: 1 }} edges={['top']}>
-        <Slot />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              flex: 1,
+              backgroundColor: '#181818',
+            },
+          }}
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              animation: 'none',
+            }}
+          />
+          <Stack.Screen
+            name="login/index"
+            options={{
+              animation: 'none',
+            }}
+          />
+          <Stack.Screen
+            name="register/index"
+            options={{
+              animation: 'slide_from_right',
+            }}
+          />
+        </Stack>
       </SafeAreaView>
     </>
   )
