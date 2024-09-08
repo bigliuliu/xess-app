98533459761b65cc23e2655c0e0a96215057c12c
Chris Yu <yuetong3yu@gmail.com>
2024-09-08 10:17:22 +1000

    feat add back button for system list page

--- app/system-list/index.tsx
+++ app/system-list/index.tsx
@@ -2,15 +2,31 @@ import { Pressable, View, Text } from 'react-native'
 import { Image } from 'expo-image'
 import { useStyles } from './useStyles'
 import { PropsWithChildren } from 'react'
+import { router } from 'expo-router'
 
 interface SystemListProps {}
 
 export default function SystemList(props: PropsWithChildren<SystemListProps>) {
   const styles = useStyles()
 
+  const handleBack = () => {
+    if (router.canGoBack()) {
+      router.back()
+    }
+  }
+
   return (
     <View style={styles.rootContainer}>
-      <Text>Notification List</Text>
+      <Pressable style={styles.backButtonWrapper} onPress={handleBack}>
+        <Image
+          source={require('@/assets/images/round_back_button.png')}
+          style={styles.backButton}
+          contentFit="contain"
+          cachePolicy="memory"
+        />
+      </Pressable>
+
+      <Text>System List</Text>
     </View>
   )
 }
--- app/system-list/useStyles.ts
+++ app/system-list/useStyles.ts
@@ -5,7 +5,24 @@ export const useStyles = () =>
   useMemo(
     () =>
       StyleSheet.create({
-        rootContainer: {},
+        rootContainer: {
+          flex: 1,
+          alignItems: 'center',
+          backgroundColor: '#181818',
+          position: 'relative',
+          paddingTop: 50,
+        },
+        backButtonWrapper: {
+          width: 40,
+          height: 40,
+          position: 'absolute',
+          top: 16,
+          left: 21,
+        },
+        backButton: {
+          width: '100%',
+          height: '100%',
+        },
       }),
     []
   )
