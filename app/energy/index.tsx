d097664babcc4c5ad552acdee06841ac90b04bdd
Chris Yu <yuetong3yu@gmail.com>
2024-09-15 18:16:17 +1000

    feat: energy title bg

--- app/energy/index.tsx
+++ app/energy/index.tsx
@@ -1,9 +1,33 @@
 import { View, Text, TextInput, Pressable, Alert, AppState } from 'react-native'
 import { useStyles } from './useStyles'
 import { CurvedBackground } from '@/components/CurvedBackground'
+import { router } from 'expo-router'
+import { Image } from 'expo-image'
 
 export default function Register() {
   const styles = useStyles()
 
-  return <View style={styles.rootContainer}></View>
+  const handleBack = () => {
+    if (router.canGoBack()) {
+      router.back()
+    }
+  }
+
+  return (
+    <View style={styles.rootContainer}>
+      <Pressable style={styles.backButtonWrapper} onPress={handleBack}>
+        <Image
+          source={require('@/assets/images/round_back_button.png')}
+          style={styles.backButton}
+          contentFit="contain"
+          cachePolicy="memory"
+        />
+      </Pressable>
+
+      <CurvedBackground styles={styles.curvedBg} />
+      <View style={styles.energyWrapper}>
+        <Text style={styles.energyTitle}>Energy</Text>
+      </View>
+    </View>
+  )
 }
